93653b9dd85013ce2489c9060592cef11e9e1754
dhrumilmk06 <kayasthadm@gmail.com>
2025-12-31 23:30:19 +0530

    signup

---
+++ BackEnd/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+
+
+export const connectDB = async () => {
+    try {
+       const conn = await mongoose.connect(process.env.MONGO_URI);
+       console.log("MONGODB CONNECTED:", conn.connection.host);
+    } catch (error) {
+        console.error("Error to Connect MongoDB:",error);
+        process.exit(1);//1 status code means fail and 0 means success
+    }
+}
