6d8a4b4fa8df3cda20f05dc3644f49af496c9128
dhrumilmk06 <kayasthadm@gmail.com>
2026-01-03 00:42:24 +0530

    message routes added

--- BackEnd/src/middlewares/arcjet.middleware.js
+++ BackEnd/src/middlewares/arcjet.middleware.js
@@ -5,15 +5,15 @@ export const arcjetProtection = async (req,res,next) => {
     try {
        const decision = await aj.protect(req);
 
-       if(decision.isDenied()){
-        if(decision.reason.isRateLimit()){
-            return res.status(429).json({message: "Rate limit exceeded. Please try again later."});
-        } else if(decision.reason.isBot()){
-            return res.status(403).json({message: "Bot access denied."});
-        }else{
-            return res.status(403).json({message: "Access denied by Security Policy."})
-        }
-       }
+    //    if(decision.isDenied()){
+    //     if(decision.reason.isRateLimit()){
+    //         return res.status(429).json({message: "Rate limit exceeded. Please try again later."});
+    //     // } else if(decision.reason.isBot()){
+    //     //     return res.status(403).json({message: "Bot access denied."});
+    //     }else{
+    //         return res.status(403).json({message: "Access denied by Security Policy."})
+    //     }
+    //    }
        
        //check for SpoofedBot
 
